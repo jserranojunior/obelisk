c58924b9edb27bb9957bbc3a219f1462b35f76d9
Jorge Serrano Junior <jorgeserranojunior@hotmail.com>
2022-09-30 22:06:02 -0300

    remove console

--- src/calculateAgeWithMonths/__tests__/calculateAgeWithMonths.test.ts
+++ src/calculateAgeWithMonths/__tests__/calculateAgeWithMonths.test.ts
@@ -1,30 +1,68 @@
 import { calculateAgeWithMonths } from "../calculateAgeWithMonths";
 describe("calcularIdadeComMeses()", () => {
-   const dateRefenceString = "2022-08-10T03:24:00"
-     const dateToRef = new Date(dateRefenceString)
-  test("calculateAgeWithMonths two months ago DATE_TWO_MONTHS (return aways = '0 anos e 2 meses')", () => {    
-              const dateForTest = new Date(dateRefenceString)  
-    const DATE_TWO_MONTHS = new Date(dateForTest.getFullYear(), dateForTest.getMonth() - 2,  dateForTest.getDay()).toLocaleDateString("fr-ca");
-    console.log(DATE_TWO_MONTHS)
-    expect(calculateAgeWithMonths(DATE_TWO_MONTHS, dateToRef)).toBe("0 anos e 2 meses");
+  const dateRefenceString = "2022-08-10T03:24:00";
+  const dateToRef = new Date(dateRefenceString);
+  test("calculateAgeWithMonths two months ago DATE_TWO_MONTHS (return aways = '0 anos e 2 meses')", () => {
+    const dateForTest = new Date(dateRefenceString);
+    const DATE_TWO_MONTHS = new Date(
+      dateForTest.getFullYear(),
+      dateForTest.getMonth() - 2,
+      dateForTest.getDay()
+    ).toLocaleDateString("fr-ca");
+    console.log(DATE_TWO_MONTHS);
+    expect(calculateAgeWithMonths(DATE_TWO_MONTHS, dateToRef)).toBe(
+      "0 anos e 2 meses"
+    );
   });
   test("calculateAgeWithMonths DATE_ON_YEAR_TWO_MONTHS (return aways = '1 ano e 1 mês')", () => {
-      const dateForTest = new Date(dateRefenceString)
-    const DATE_ON_YEAR_TWO_MONTHS = new Date(dateForTest.getFullYear() -1, dateForTest.getMonth() - 1, dateForTest.getDay()).toLocaleDateString("fr-ca");
-    expect(calculateAgeWithMonths(DATE_ON_YEAR_TWO_MONTHS, dateToRef)).toBe("1 ano e 1 mês");
+    const dateForTest = new Date(dateRefenceString);
+    const DATE_ON_YEAR_TWO_MONTHS = new Date(
+      dateForTest.getFullYear() - 1,
+      dateForTest.getMonth() - 1,
+      dateForTest.getDay()
+    ).toLocaleDateString("fr-ca");
+    expect(calculateAgeWithMonths(DATE_ON_YEAR_TWO_MONTHS, dateToRef)).toBe(
+      "1 ano e 1 mês"
+    );
   });
-    test("calculateAgeWithMonths DATE_ON_YEAR_TWO_MONTHS (return aways = '1 ano e 2 meses')", () => {
-      const dateForTest = new Date(dateRefenceString)
-    const DATE_ON_YEAR_TWO_MONTHS = new Date(dateForTest.getFullYear() -1, dateForTest.getMonth() - 2, dateForTest.getDay()).toLocaleDateString("fr-ca");
-    expect(calculateAgeWithMonths(DATE_ON_YEAR_TWO_MONTHS, dateToRef)).toBe("1 ano e 2 meses");
+  test("calculateAgeWithMonths DATE_ON_YEAR_TWO_MONTHS (return aways = '1 ano e 2 meses')", () => {
+    const dateForTest = new Date(dateRefenceString);
+    const DATE_ON_YEAR_TWO_MONTHS = new Date(
+      dateForTest.getFullYear() - 1,
+      dateForTest.getMonth() - 2,
+      dateForTest.getDay()
+    ).toLocaleDateString("fr-ca");
+    expect(calculateAgeWithMonths(DATE_ON_YEAR_TWO_MONTHS, dateToRef)).toBe(
+      "1 ano e 2 meses"
+    );
   });
-    test("calculateAgeWithMonths DATE_ON_YEAR_TWO_MONTHS_ONE_DAY (return aways = '1 anos e 3 meses')", () => {
-        const dateForTest = new Date(dateRefenceString)
-    const DATE_ON_YEAR_TWO_MONTHS_ONE_DAY = new Date(dateForTest.getFullYear() -1, dateForTest.getMonth() - 3, dateForTest.getDay() + 10).toLocaleDateString("fr-ca");
-    expect(calculateAgeWithMonths(DATE_ON_YEAR_TWO_MONTHS_ONE_DAY, dateToRef)).toBe("1 ano e 3 meses");
+  test("calculateAgeWithMonths DATE_ON_YEAR_TWO_MONTHS_ONE_DAY (return aways = '1 anos e 3 meses')", () => {
+    const dateForTest = new Date(dateRefenceString);
+    const DATE_ON_YEAR_TWO_MONTHS_ONE_DAY = new Date(
+      dateForTest.getFullYear() - 1,
+      dateForTest.getMonth() - 3,
+      dateForTest.getDay() + 10
+    ).toLocaleDateString("fr-ca");
+    expect(
+      calculateAgeWithMonths(DATE_ON_YEAR_TWO_MONTHS_ONE_DAY, dateToRef)
+    ).toBe("1 ano e 3 meses");
   });
   test("calculateAgeWithMonths DATE_FORTY (return aways = '76 anos e 11 meses')", () => {
     const DATE_FORTY = "1945-08-11";
-    expect(calculateAgeWithMonths(DATE_FORTY, dateToRef)).toBe("76 anos e 11 meses");
+    expect(calculateAgeWithMonths(DATE_FORTY, dateToRef)).toBe(
+      "76 anos e 11 meses"
+    );
+  });
+  test("calculateAgeWithMonths DATE_USER (return aways = '44 anos e 7 meses')", () => {
+    const DATE_USER = "1978-01-05";
+    expect(calculateAgeWithMonths(DATE_USER, dateToRef)).toBe(
+      "44 anos e 7 meses"
+    );
+  });
+  test("calculateAgeWithMonths DATE_USER (return aways = '37 anos e 1 mês')", () => {
+    const DATE_USER = "1985-07-18";
+    expect(calculateAgeWithMonths(DATE_USER, dateToRef)).toBe(
+      "37 anos e 1 mês"
+    );
   });
 });
--- src/calculateAgeWithMonths/calculateAgeWithMonths.ts
+++ src/calculateAgeWithMonths/calculateAgeWithMonths.ts
@@ -1,65 +1,70 @@
 import internal from "stream";
 
 export function calculateAgeWithMonths(value: any, dateJsToCompare: Date) {
-const actualDate = dateJsToCompare
-      const ano:number = calculateYearsOfLife(value, actualDate);
-  const meses:number = calculateMonthOfLife(value,dateJsToCompare);
-  const IDADE = {ano: ano, mes: meses}
+  const actualDate = dateJsToCompare;
+  const ano: number = calculateYearsOfLife(value, actualDate);
+  const meses: number = calculateMonthOfLife(value, dateJsToCompare);
+  const IDADE = { ano: ano, mes: meses };
 
-      return formatNameYearsMonth(IDADE) 
-
+  return formatNameYearsMonth(IDADE);
 }
-export function calculateYearsOfLife(birthToCalculate: string, dateJsToCompare: Date) {
+export function calculateYearsOfLife(
+  birthToCalculate: string,
+  dateJsToCompare: Date
+) {
   if (birthToCalculate.length === 10) {
     const birthdate = new Date(birthToCalculate);
-    const yearsInt= dateJsToCompare.getTime() - birthdate.getTime();
+    const yearsInt = dateJsToCompare.getTime() - birthdate.getTime();
     const yearsInDate = new Date(yearsInt);
     let yearsOld = Math.abs(yearsInDate.getUTCFullYear() - 1970);
-    if (compareMonthWithActualDate(birthToCalculate,dateJsToCompare) == "smaller") {
-      yearsOld - 1
+    if (
+      compareMonthWithActualDate(birthToCalculate, dateJsToCompare) == "smaller"
+    ) {
+      yearsOld - 1;
     }
     return yearsOld;
-  }else{
-    return 0
+  } else {
+    return 0;
   }
 }
 
-export function formatNameYearsMonth(idade:{ano:number, mes:number}){
+export function formatNameYearsMonth(idade: { ano: number; mes: number }) {
+  let nameMonth = "meses";
+  let nameYear = "anos";
 
-  let nameMonth = "meses"
-  let nameYear = "anos"
-
-  if(idade.mes == 1){
-    nameMonth = "mês"
+  if (idade.mes == 1) {
+    nameMonth = "mês";
   }
-  if (idade.ano == 1){
-    nameYear  = "ano"
+  if (idade.ano == 1) {
+    nameYear = "ano";
   }
-    return `${idade.ano} ${nameYear} e ${idade.mes } ${nameMonth}`;
+  return `${idade.ano} ${nameYear} e ${idade.mes} ${nameMonth}`;
 }
-export function compareMonthWithActualDate(birthToCalculate: string, dateJsToCompare: Date){
-      const birthdate = new Date(birthToCalculate);
-      const dateActual = dateJsToCompare
-      const monthActual = dateActual.getMonth()
-      const monthBirth = birthdate.getMonth();
-      if (monthBirth > monthActual) {
-        return "larger"
-      }
-        else if(monthBirth < monthActual) {
-                    console.log(dateActual, birthdate, monthBirth, monthActual)
-          return "smaller"
-        }
-        else if(monthBirth == monthActual) {
-
-          return "equal"
-        }
+export function compareMonthWithActualDate(
+  birthToCalculate: string,
+  dateJsToCompare: Date
+) {
+  const birthdate = new Date(birthToCalculate);
+  const dateActual = dateJsToCompare;
+  const monthActual = dateActual.getMonth();
+  const monthBirth = birthdate.getMonth();
+  if (monthBirth > monthActual) {
+    return "larger";
+  } else if (monthBirth < monthActual) {
+    return "smaller";
+  } else if (monthBirth == monthActual) {
+    return "equal";
+  }
 }
-export function calculateMonthOfLife(birthToCalculate: string, dateJsToCompare: Date) {
+export function calculateMonthOfLife(
+  birthToCalculate: string,
+  dateJsToCompare: Date
+) {
   if (birthToCalculate.length === 10) {
     const birthdate = new Date(birthToCalculate);
-    const dateActual = dateJsToCompare
-    const monthActual = dateActual.getMonth()
-        const monthBirth = birthdate.getMonth();
+    const dateActual = dateJsToCompare;
+    const monthActual = dateActual.getMonth();
+    const monthBirth = birthdate.getMonth();
     const dayActual = dateActual.getDate();
 
     const dayBirth = birthdate.getDate();
@@ -67,25 +72,27 @@ export function calculateMonthOfLife(birthToCalculate: string, dateJsToCompare: 
     let monthCont = 0;
     let mesesFalta = 0;
 
-    if (compareMonthWithActualDate(birthToCalculate, dateJsToCompare) == "larger") {
+    if (
+      compareMonthWithActualDate(birthToCalculate, dateJsToCompare) == "larger"
+    ) {
       mesesFalta = monthBirth - monthActual;
       monthCont = mesesFalta;
-    }else  if (compareMonthWithActualDate(birthToCalculate, dateJsToCompare) == "smaller") {
+    } else if (
+      compareMonthWithActualDate(birthToCalculate, dateJsToCompare) == "smaller"
+    ) {
       mesesFalta = monthActual - monthBirth;
-      monthCont = mesesFalta
-    } else  if (compareMonthWithActualDate(birthToCalculate, dateJsToCompare) == "equal") {
-     
+      monthCont = mesesFalta;
+    } else if (
+      compareMonthWithActualDate(birthToCalculate, dateJsToCompare) == "equal"
+    ) {
       if (dayBirth > dayActual) {
-      monthCont = 0;
-      }else{
-         monthCont = 11;
+        monthCont = 0;
+      } else {
+        monthCont = 11;
       }
     }
     return monthCont;
-  }else{
-    return 0
+  } else {
+    return 0;
   }
 }
-// 08 > 07
-// 06 - 7 = 1
-
